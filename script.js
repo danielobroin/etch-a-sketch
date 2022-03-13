4fa1f60943363f55694c0fef7472cee1f9cbe11f
Daniel O Broin <danielobroin@gmail.com>
2022-03-13 19:32:25 +0000

    Added grid and colour graident
    No interactivity yet.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-for (i = 0; i<16; i++){
 
-    
-    
+let body = document.querySelector("body")
+let container = document.querySelector("#container")
 
+
+let size = 10;
+let i = 0;
+let k = 0;
+for (i = 0; i <=size; i++){
+    container.setAttribute('style', `grid-template-columns: repeat(${i}, 1fr); grid-template-rows: repeat(${i}, 1fr);`);
+
+
+    for (k = 0; k < size; k++){
+        let square = document.createElement("div");
+       /* let cover = document.creaeteElement("div")*/
+        square.style.backgroundColor = `rgb(${i*(255/size)}, ${k*(255/size)}, 255)`;
+        /*square.style.border =*/
+       
+        container.appendChild(square)
+       
+        square.addEventListener("mouseover", function() {})
+    }
 }
