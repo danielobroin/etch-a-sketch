55c6925a84ddf061dcacff38ae075dc6d48f4bb3
Daniel O Broin <danielobroin@gmail.com>
2022-03-15 00:17:35 +0000

    Added borders, fixed grid layout.

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ let container = document.querySelector("#container")
 let size = 10;
 let i = 0;
 let k = 0;
-for (i = 0; i <=size; i++){
-    container.setAttribute('style', `grid-template-columns: repeat(${i}, 1fr); grid-template-rows: repeat(${i}, 1fr);`);
+for (i = 0; i <size; i++){
+    container.setAttribute('style', `grid-template-columns: repeat(${i+1}, 1fr); grid-template-rows: repeat(${i+1}, 1fr);`);
 
 
     for (k = 0; k < size; k++){
         let square = document.createElement("div");
-       /* let cover = document.creaeteElement("div")*/
+        square.style.border = 'solid';
+        square.style.borderColor = 'white';
         square.style.backgroundColor = `rgb(${i*(255/size)}, ${k*(255/size)}, 255)`;
-        /*square.style.border =*/
-       
+        
+        square.classList.add('cover');
         container.appendChild(square)
        
         square.addEventListener("mouseover", function() {})
