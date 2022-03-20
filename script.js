0a588763decd2c33a442410883858e4b8c9cd3bd
Daniel O Broin <danielobroin@gmail.com>
2022-03-20 23:23:50 +0000

    Added background color change on mouseover
    Added background color change on mouseover with color gradient
    acoss the grid.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let container = document.querySelector("#container")
 let size = 10;
 let i = 0;
 let k = 0;
-for (i = 0; i <size; i++){
+for (i = 0; i <=size; i++){
     container.setAttribute('style', `grid-template-columns: repeat(${i+1}, 1fr); grid-template-rows: repeat(${i+1}, 1fr);`);
 
 
@@ -14,11 +14,9 @@ for (i = 0; i <size; i++){
         let square = document.createElement("div");
         square.style.border = 'solid';
         square.style.borderColor = 'white';
-        square.style.backgroundColor = `rgb(${i*(255/size)}, ${k*(255/size)}, 255)`;
-        
-        square.classList.add('cover');
-        container.appendChild(square)
+        square.setAttribute('data-color', `rgb(${i*(255/size)}, ${k*(255/size)}, 255)`);
+        container.appendChild(square);
        
-        square.addEventListener("mouseover", function() {})
+        square.addEventListener("mouseover", function() {square.style.backgroundColor = square.getAttribute('data-color')})
     }
 }
