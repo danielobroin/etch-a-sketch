c0b6d0c5c70073dbef7159a85e7cbabba3bcdb42
Daniel O Broin <danielobroin@gmail.com>
2022-03-27 22:38:07 +0100

    Implemented slider function.
    Made slider function and container contents reset so that new divs
    aren't just added to the existing ones.
    Finished with etchasketch project for now.

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 
 let body = document.querySelector("body")
 let container = document.querySelector("#container")
+size = 5;
+etchasketch();
+let slider = document.querySelector(".slider");
 
+slider.oninput = function(){
+    size = this.value;
+    etchasketch();
+}
 
-let size = 10;
-for (i = 0; i <=size; i++){
-    container.setAttribute('style', `grid-template-columns: repeat(${i+1}, 1fr); grid-template-rows: repeat(${i+1}, 1fr);`);
-
-
-    for (k = 0; k < size; k++){
-        let square = document.createElement("div");
-        square.style.border = 'solid';
-        square.style.borderColor = 'white';
-        square.setAttribute('data-color', `rgb(${i*(255/size)}, ${k*(255/size)}, 255)`);
-        container.appendChild(square);
-        square.addEventListener("mouseover", function() {square.style.backgroundColor = square.getAttribute('data-color')})
+function etchasketch() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
     }
+
+    for (i = 0; i <size; i++){
+        container.setAttribute('style', `grid-template-columns: repeat(${i+1}, 1fr); grid-template-rows: repeat(${i+1}, 1fr);`);
+
+
+        for (k = 0; k < size; k++){
+            let square = document.createElement("div");
+            square.style.border = 'solid';
+            square.style.borderColor = 'white';
+            square.setAttribute('data-color', `rgb(${i*(255/size)}, ${k*(255/size)}, 255)`);
+            container.appendChild(square);
+            square.addEventListener("mouseover", function() {square.style.backgroundColor = square.getAttribute('data-color')})
+        }
+    }
+AuthenticatorAssertionResponse
 }
